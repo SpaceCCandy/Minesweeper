d0b16c56c79eb989e17dbd76faeaabbf52d53262
JamishaBade <badejamishagmail.com>
2025-08-07 14:57:39 -0400

    newupdate

--- lookupTable.c
+++ lookupTable.c
@@ -25,37 +25,3 @@ typedef enum
   DIR_LEFT = 2,
   DIR_RIGHT = 3
 } Direction;
-
-uint32_t getTypeColor(ItemType type)
-{
-  return (uint32_t)type;
-}
-
-const char *getTypeName(ItemType type)
-{
-  switch (type)
-  {
-  case TYPE_SELECT:
-    return "SELECT";
-  case TYPE_BOMB:
-    return "BOMB";
-  case TYPE_ONE:
-    return "ONE";
-  case TYPE_TWO:
-    return "TWO";
-  case TYPE_THREE:
-    return "THREE";
-  case TYPE_FOUR:
-    return "FOUR";
-  case TYPE_FIVE:
-    return "FIVE";
-  case TYPE_SIX:
-    return "SIX";
-  case TYPE_SEVEN:
-    return "SEVEN";
-  case TYPE_EIGHT:
-    return "EIGHT";
-  default:
-    return NULL;
-  }
-}
