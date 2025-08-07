e1e9fa7cb34f1453394364397167ba0ad87c16db
JamishaBade <badejamishagmail.com>
2025-08-07 14:03:34 -0400

    lookup table for directions and color hex values

--- lookupTable.c
+++ lookupTable.c
@@ -1,33 +1,22 @@
 #include <stdint.h>
-#define ERROR_COLOR 0x000000
 #include <stddef.h>
+
+#define ERROR_COLOR 0x000000
+#define ITEM_TYPE_COUNT 10
+
 typedef enum
 {
-  TYPE_SELECT, // White
-  TYPE_BOMB,   // Red
-  TYPE_ONE,    // Blue
-  TYPE_TWO,    // Green
-  TYPE_THREE,  // Yellow
-  TYPE_FOUR,   // Pink
-  TYPE_FIVE,   // Orange
-  TYPE_SIX,    // Cyan
-  TYPE_SEVEN,  // Purple
-  TYPE_EIGHT,  // Brown
-  TYPE_COUNT
+  TYPE_SELECT = 0xFFFFFF, // White
+  TYPE_BOMB = 0xFE0000,   // Red
+  TYPE_ONE = 0x4363D8,    // Blue
+  TYPE_TWO = 0x3CB44B,    // Green
+  TYPE_THREE = 0xFFCA00,  // Yellow
+  TYPE_FOUR = 0xF032E6,   // Pink
+  TYPE_FIVE = 0xF58231,   // Orange
+  TYPE_SIX = 0x46F0F0,    // Cyan
+  TYPE_SEVEN = 0x5F530F,  // Purple
+  TYPE_EIGHT = 0x5F530F   // Brown
 } ItemType;
-
-const uint32_t typeColors[] = {
-    0xFFFFFF, // SELECT -> White
-    0xFE0000, // BOMB -> Red
-    0x4363D8, // ONE -> Blue
-    0x3CB44B, // TWO -> Green
-    0xFFCA00, // THREE -> Yellow
-    0xF032E6, // FOUR -> Pink
-    0xF58231, // FIVE -> Orange
-    0x46F0F0, // SIX -> Cyan
-    0x8800DD, // SEVEN -> Purple
-    0x5F530F  // EIGHT -> Brown
-};
 
 typedef enum
 {
@@ -39,21 +28,34 @@ typedef enum
 
 uint32_t getTypeColor(ItemType type)
 {
-  if (type < TYPE_COUNT)
-  {
-    return typeColors[type];
-  }
-  return ERROR_COLOR;
+  return (uint32_t)type;
 }
 
 const char *getTypeName(ItemType type)
 {
-  const char *names[] = {
-      "SELECT", "BOMB", "ONE", "TWO", "THREE",
-      "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT"};
-  if (type < TYPE_COUNT)
+  switch (type)
   {
-    return names[type];
+  case TYPE_SELECT:
+    return "SELECT";
+  case TYPE_BOMB:
+    return "BOMB";
+  case TYPE_ONE:
+    return "ONE";
+  case TYPE_TWO:
+    return "TWO";
+  case TYPE_THREE:
+    return "THREE";
+  case TYPE_FOUR:
+    return "FOUR";
+  case TYPE_FIVE:
+    return "FIVE";
+  case TYPE_SIX:
+    return "SIX";
+  case TYPE_SEVEN:
+    return "SEVEN";
+  case TYPE_EIGHT:
+    return "EIGHT";
+  default:
+    return NULL;
   }
-  return NULL;
 }
